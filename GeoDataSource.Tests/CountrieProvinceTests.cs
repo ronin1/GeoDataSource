38684d951a3c48301777e7aabf13ae819f2a06d8
Huy Nguyen <huy@ronin.ws>
2015-11-02 11:02:59 -0800

    Updated unit tests to use cleaner test cases

---
+++ GeoDataSource.Tests/CountrieProvinceTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GeoDataSource.Tests
+{
+    [TestFixture]
+    public class CountrieProvinceTests
+    {
+
+        [Test]
+        public void GetCountries()
+        {
+            var countries = GeoData.Current.Countries;
+            Assert.IsNotNull(countries);
+            Assert.Greater(countries.Count(), 200);
+            countries.ForEach(c =>
+            {
+                Assert.IsNotNull(c);
+                CollectionAssert.IsNotEmpty(c.ContinentId, c.Name);
+                CollectionAssert.IsNotEmpty(c.Name);
+            });
+        }
+
+        [TestCase("124", ExpectedResult = "Canada")]
+        [TestCase("CA", ExpectedResult = "Canada")]
+        [TestCase("CAN", ExpectedResult = "Canada")]
+        [TestCase("US", ExpectedResult = "United States")]
+        [TestCase("USA", ExpectedResult = "United States")]
+        [TestCase("United States", ExpectedResult = "United States")]
+        [TestCase("JP", ExpectedResult = "Japan")]
+        [TestCase("Japan", ExpectedResult = "Japan")]
+        [TestCase("JPN", ExpectedResult = "Japan")]
+        [TestCase("3R!", ExpectedException = typeof(NullReferenceException))]
+        [TestCase("#R!", ExpectedException = typeof(NullReferenceException))]
+        [TestCase(null, ExpectedException = typeof(NullReferenceException))]
+        public string GetCountry(string name)
+        {
+            Country c = GeoData.Current.GetCountry(name);
+            return c.Name;
+        }
+
+        [TestCase("JP", "Aichi-Ken", ExpectedResult = true)]
+        [TestCase("US", "New York", ExpectedResult = true)]
+        [TestCase("US", "California", ExpectedResult = true)]
+        [TestCase("CA", "British Columbia", ExpectedResult = true)]
+        public bool CountryHasProvince(string country, string province)
+        {
+            var c = GeoData.Current.GetCountry(country);
+            var provinces = GeoData.Current.ProvincesByCountry(c);
+            return provinces.Any(p => string.Compare(p.Name, province, true) == 0);
+        }
+
+        [TestCase("US", "New York", ExpectedResult = "America/New_York")]
+        [TestCase("US", "California", ExpectedResult = "America/Los_Angeles")]
+        [TestCase("CA", "British Columbia", ExpectedResult = "America/Vancouver")]
+        public string GetProvinceBCTimeZone(string country, string province)
+        {
+            var c = GeoData.Current.GetCountry(country);
+            var pp = GeoData.Current.ProvincesByCountry(c);
+            var prov = (from p in pp
+                        where string.Compare(p.Name, province, true) == 0
+                        select p).FirstOrDefault();
+
+            var tz = GeoData.Current.TimeZone(prov);
+            return tz.TimeZoneId;
+        }
+
+        [TestCase("US", "New York", ExpectedResult = -5.0d)]
+        [TestCase("US", "California", ExpectedResult = -8.0d)]
+        [TestCase("CA", "British Columbia", ExpectedResult = -8.0d)]
+        public double GetProvinceTimeGmtOffset(string country, string province)
+        {
+            var c = GeoData.Current.GetCountry(country);
+            var pp = GeoData.Current.ProvincesByCountry(c);
+            var prov = (from p in pp
+                      where string.Compare(p.Name, province, true) == 0
+                      select p).FirstOrDefault();
+
+            var tz = GeoData.Current.TimeZone(prov);
+            return tz.GMTOffSet;
+        }
+
+        [TestCase("Mexico", ExpectedResult = 52)]
+        [TestCase("Canada", ExpectedResult = 1)]
+        [TestCase("United States", ExpectedResult = 1)]
+        public int USAPhones(string country)
+        {
+            var phones = PhoneManager.Current.AllByCountry(country);
+            CollectionAssert.IsNotEmpty(phones);
+            return phones.First().CountryCode;
+        }
+
+
+    }
+}
--- GeoDataSource.Tests/ValidationTests.cs
+++ GeoDataSource.Tests/ValidationTests.cs
@@ -10,57 +10,48 @@ namespace GeoDataSource.Tests
     [TestFixture]
     public class ValidationTests
     {
-        [Test]
-        public void CanadaValdiationWithSpaceInPostalCode()
+        [TestCase("CA", "", ExpectedResult = false)]
+        [TestCase("CA", "BLAH", ExpectedResult = false)]
+        [TestCase("CA", "V5B 2Y2", ExpectedResult = true)]
+        [TestCase("CA", "V5B2Y2", ExpectedResult = true)]
+        [TestCase("US", "90025", ExpectedResult = true)]
+        [TestCase("US", "90401", ExpectedResult = true)]
+        [TestCase("US", "90405", ExpectedResult = true)]
+        [TestCase("US", "90210", ExpectedResult = true)]
+        [TestCase("US", "1111", ExpectedResult = false)]
+        [TestCase("US", "999999", ExpectedResult = false)]
+        [TestCase("US", "9999z", ExpectedResult = false)]
+        [TestCase("US", "", ExpectedResult = false)]
+        [TestCase("", "", ExpectedResult = false)]
+        public bool CanadaValdiationWithSpaceInPostalCode(string country, string postal)
         {
-
-            Assert.IsTrue(GeoData.Current.ValidatePostalCodeByCountry("CA", "V5B 2Y2"));
-        }
-        [Test]
-        public void CanadaValdiationWithNoSpaceInPostalCode()
-        {
-
-            Assert.IsTrue(GeoData.Current.ValidatePostalCodeByCountry("CA", "V5B2Y2"));
-
-        }
-        [Test]
-        public void USAValdiationWith5Digit()
-        {
-
-            Assert.IsTrue(GeoData.Current.ValidatePostalCodeByCountry("USA", "90210"));
+            return GeoData.Current.ValidatePostalCodeByCountry(country, postal);
         }
 
-        [Test]
-        public void USAPhoneTest()
+        [TestCase("", ExpectedException = typeof(NullReferenceException))]
+        [TestCase("111", ExpectedException = typeof(NullReferenceException))]
+        [TestCase("254(70sdf0) 669!@#$%^&*()_024", ExpectedException = typeof(NullReferenceException))]
+        [TestCase("1(604) 338 2512", ExpectedResult = "Canada")]
+        [TestCase("52-1-(81) 8309-6670", ExpectedResult = "Mexico")]
+        [TestCase("254(700) 669024", ExpectedResult = "Kenya")]
+        [TestCase("52-55-12345678", ExpectedResult = "Mexico")]
+        [TestCase("1-626-123-4567", ExpectedResult = "United States")]
+        [TestCase("1(310) 123-4567", ExpectedResult = "United States")]
+        public string PhoneDetection(string phone)
         {
-            Assert.IsNotNull(PhoneManager.Current.AutoDetect("1(310) 652-4106"));
-        }
-        [Test]
-        public void BCCanadaPhoneTest()
-        {
-            var phone = PhoneManager.Current.AutoDetect("1(604) 338 2512");
-            Assert.AreEqual("Canada", phone.Country);
-            StringAssert.StartsWith("British Columbia", phone.Comment);
-        }
-
-        [Test]
-        public void MexicoPhoneTest()
-        {
-            var phone = PhoneManager.Current.AutoDetect("52-1-(81) 8309-6670");
-            Assert.AreEqual("Mexico", phone.Country);
-        }
-        [Test]
-        public void KenyaPhoneTest()
-        {
-            var phone = PhoneManager.Current.AutoDetect("254(700) 669024");
-            Assert.AreEqual("Kenya", phone.Country);
+            var p = PhoneManager.Current.AutoDetect(phone);
+            return p.Country;
         }
 
-        [Test]
-        public void Crappy_Input_Wont_Return_A_Good_ResultTest()
+        [TestCase("1(310) 123-4567", "United States", "Santa Monica")]
+        [TestCase("1(310) 123-4567", "United States", "Los Angeles")]
+        [TestCase("1(604) 338 2512", "Canada", "British Columbia")]
+        public void BCCanadaPhoneTest(string phone, string expectCountry, string expectProvince)
         {
-            var phone = PhoneManager.Current.AutoDetect("254(70sdf0) 669!@#$%^&*()_024");
-            Assert.IsNull(phone);
+            var p = PhoneManager.Current.AutoDetect(phone);
+            StringAssert.AreEqualIgnoringCase(expectCountry, p.Country);
+            StringAssert.Contains(expectProvince.ToLower(), p.Comment.ToLower());
         }
+        
     }
 }
